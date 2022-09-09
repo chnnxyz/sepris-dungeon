36a4f269131ffaa1e4a0ec8cf5f7e06484977f90
chnnxyz <s@chnn.xyz>
2022-09-08 22:15:01 -0500

    Drawing emptyspace

--- src/dungeon/grid.py
+++ src/dungeon/grid.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 from src.rooms.room import Room
+from src.rooms.control_rooms import EmptySpace
 
 class Grid:
     def __init__(self, 
@@ -19,8 +20,8 @@ class Grid:
 
         for room in rooms:
             # Control for cases where x or y may get repeated
-            if self.base_grid[room.x, room.y] is not None:
-                self.base_grid[room.x,room.y] = room
+            if self.base_grid[room.x][room.y] is None:
+                self.base_grid[room.x][room.y] = room
             else:
                 for row in self.base_grid:
                     try:
@@ -28,6 +29,12 @@ class Grid:
                         self.base_grid[row,emptycol] = room
                     except:
                         continue
+        
+        for i in range(len(self.base_grid)):
+            for j in range(len(self.base_grid[i])):
+                if self.base_grid[i][j] is None:
+                    self.base_grid[i][j] = EmptySpace('empty',x=i,y=j)
+
 
     def _check_colission(self):
         pass
@@ -38,5 +45,42 @@ class Grid:
     def move_room(self, direction:str, x:int, y:int):
         pass
 
+    def draw_room(self,room:Room,g:str):
+        lines = g.split("\n")
+        if isinstance(room,EmptySpace):
+            for i in range(3):
+                lines[(4*room.y)+i+1] = (lines[(4*room.y)+i+1][:10*room.x +1] + 
+                                     "x"*9 +
+                                     lines[(4*room.y)+i+1][10*(room.x +1):])
+        out = '\n'.join(lines)
+        return out
+
+                
+
+
     def draw_grid(self):
-        pass
+        base_drawing = '''
++---------+---------+---------+
+|         |         |         |
+|         |         |         |
+|         |         |         | 
++---------+---------+---------+
+|         |         |         |
+|         |         |         |
+|         |         |         | 
++---------+---------+---------+
+|         |         |         |
+|         |         |         |
+|         |         |         | 
++---------+---------+---------+
+'''
+        #janky fix
+        base_drawing = base_drawing.split('\n')
+        base_drawing = base_drawing[1:]
+        base_drawing = "\n".join(base_drawing)
+        for i in range(len(self.base_grid)):
+            for j in range(len(self.base_grid[i])):
+                base_drawing = self.draw_room(self.base_grid[i][j], 
+                                              base_drawing)
+
+        print(base_drawing)
--- src/rooms/control_rooms.py
+++ src/rooms/control_rooms.py
@@ -1,14 +1,17 @@
 from src.rooms.room import Room
-from src.dungeon.grid import Grid
 from typing import List, Tuple
 
 #TODO: add gravity methods
 class GravityRoom(Room):
     
-    def apply_gravity(self,direction:str,grid:Grid):
-        grid.shift_grid(direction)
+    def apply_gravity(self):
+        pass
 
 class PanelRoom(Room):
 
-    def use_joystick(self,direction:str,grid:Grid,x:int,y:int):
-        grid.move_room(direction,x,y)
+    def use_joystick(self):
+        pass
+
+class EmptySpace(Room):
+
+    pass
